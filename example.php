7814c414c021d7e99fc109ec6fb7a233d233491f
koli mondol mira <koli15-1880@diu.edu.bd>
2021-04-21 20:38:17 +0600

    second commit

---
+++ example.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+student_id: <input type="text" name="student_id"><br>
+section: <input type="text" name="section"><br>
+
+
+<input type="submit">
+</form>
+</body>
+</html>
